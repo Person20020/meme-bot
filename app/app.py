1e6689061dc4838da3019450e4e5d523bef2f48c
Koji Ino <kojiino@outlook.com>
2025-07-18 18:04:58 -1000

    added start

--- app/app.py
+++ app/app.py
@@ -121,3 +121,7 @@ def handle_commands():
             user=user_id,
             text="An unknown command somehow triggered this bot." 
         )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=3003)
